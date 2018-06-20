7bd8b8f959dd452569691148e7fbed3e7c03986f
Pablo Castelnovo <pablonvz@gmail.com>
2018-06-19 21:35:55 -0400

    Initial commit

---
+++ src/main/java/com/pablocastelnovo/muslim_gregorian_date_conversions_showcase/App.java
@@ -0,0 +1,11 @@
+package com.pablocastelnovo.muslim_gregorian_date_conversions_showcase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/pablocastelnovo/muslim_gregorian_date_conversions_showcase/AppTests.java
@@ -0,0 +1,16 @@
+package com.pablocastelnovo.muslim_gregorian_date_conversions_showcase;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTests {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
